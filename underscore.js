09bd21acb1b87cec5e73e027645ec1a43ac91d8f
chanlumin <chanlumin@163.com>
2018-01-31 11:49:45 +0800

    添加random函数

--- underscore.js
+++ underscore.js
@@ -23,7 +23,7 @@
   nativeBind = FuncProto.bind,
   nativeCreate = Object.create;
 
-
+  // 无new实例化
   var _ = function(obj) {
     if(obj instanceof _) return obj
     if(!(this instanceof _)) return new _(obj)
@@ -37,6 +37,31 @@
     root._ = _
   }
 
+  // 静态api
+  /**
+   * 空函数 用来减少填充接口时 多次使用空函数的时候的开销
+   */
+  _.noop = function () {}
+  
+  _.random = function (min, max) {
+    // 处理整数
+    min = Math.floor(min)
+    max = Math.floor(max)
+
+    // 处理只有一个参数的问题
+    if(max === null) {
+      max = min
+      min = 0
+    } else if(min > max) { // 处理min和max传递相反的问题
+      var temp = min
+      min = max
+      max = min
+    }
+
+    return  min + Math.floor(Math.random() * (max - min + 1))
+  }
+
+  // 实例方法
   _.prototype.value = function () {
     return this._wrapped
   }
